5fce06482d8a4ec24aaa9241e2b8f0b8bcf63ebd
JOVE <jove@itu.dk>
2020-02-28 11:31:02 +0100

    created css file

--- joindenmark/src/components/Tile/Tile.js
+++ joindenmark/src/components/Tile/Tile.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
+import "./Tile.css";
 
 export function Tile(props) {
   const [title, setTitle] = useState(props.state.title);
 
   return (
-    <div>
+    <div className="tile">
       <h2>{title}</h2>
     </div>
   );
--- joindenmark/src/frontpage/FrontPage.js
+++ joindenmark/src/frontpage/FrontPage.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
-import {Tile} from '../components/Tile';
+import {Tile} from '../components/Tile/Tile';
 
 export function FrontPage() {
   return (
     <div>
       <h1>Welcome to Join Denmark</h1>
-      <Tile state={{title:'test'}}/>
+      <Tile state={{title:'Public Sector'}}/>
+      <Tile state={{title:'Accommodation'}}/>
+      <Tile state={{title:'Job Market'}}/>
+      <Tile state={{title:'Culture'}}/>
     </div>
   );
 }
