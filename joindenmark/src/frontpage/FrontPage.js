2f7317f4e7f4bc1b1b496e4a4f0b866c016ebd8e
JOVE <jove@itu.dk>
2020-02-28 11:22:13 +0100

    Minor fix

--- joindenmark/src/frontpage/FrontPage.js
+++ joindenmark/src/frontpage/FrontPage.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
-import {Tile} from './components/Tile';
+import {Tile} from '../components/Tile';
 
 export function FrontPage() {
   return (
     <div>
       <h1>Welcome to Join Denmark</h1>
-      <Tile state={{title='test'}}/>
+      <Tile state={{title:'test'}}/>
     </div>
   );
 }
