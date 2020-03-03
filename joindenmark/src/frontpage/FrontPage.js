9f3e7ae827023051da609491ec2194757e55d505
JOVE <jove@itu.dk>
2020-03-03 16:06:31 +0100

    tiles works

--- joindenmark/src/frontpage/FrontPage.js
+++ joindenmark/src/frontpage/FrontPage.js
@@ -1,53 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Tile } from "../components/tile/Tile";
 import { BrowserRouter as Router, Link } from "react-router-dom";
 import {db} from "../firebase"
 
-const dataFromDB = db.collection("tile").get();
-const tiles = [];
-
-function convertToPath(title){
-  return "/" + title.replace(/ /g,'').toLowerCase()
-}
-
-function makeTiles(){
-  dataFromDB.then(function(querySnapshot) {
-    querySnapshot.forEach(function(doc) {
-      console.log(doc.id, " => ", doc.data());
-      tiles.push(
-        <Link
-          to={{
-            pathname: convertToPath(doc.data().title),
-            state: { title: doc.data().title, picture: doc.data().picture }
-          }}
-        >
-          <Tile
-            state={{ title: doc.data().title, picture: doc.data().picture }}
-          />
-      </Link>
-        );
-    });
-  });
-  console.log(tiles);
-  return tiles;
-}
-
 export function FrontPage() {
+  const dataFromDB = db.collection("tile").get();
+  const [tiles, setTiles] = useState([]) ;
+  
+  function convertToPath(title){
+    return "/" + title.replace(/ /g,'').toLowerCase()
+  }
+  
+  function makeTiles(){
+    dataFromDB.then(function(querySnapshot) {
+      const arr = [];
+      querySnapshot.forEach(function(doc) {
+        arr.push(
+          <Link key={doc.data().title}
+            to={{
+              pathname: convertToPath(doc.data().title),
+              state: { title: doc.data().title, picture: doc.data().picture }
+            }}
+          >
+            <Tile
+              state={{ title: doc.data().title, picture: doc.data().picture }}
+            />
+        </Link>);
+      });
+      setTiles(arr);
+    });
+  }
+
+  useEffect(() => {
+    makeTiles();
+  },[]);
+
   return (    
     <div>
-    
-      {makeTiles()}
-    
       <h1>Join Denmark - step by step</h1>
-      
-      <Link
-        to={{
-          pathname: "/culture",
-          state: { title: "Culture", picture: "/images/culture.jpg" }
-        }}
-      >
-        <Tile state={{ title: "Culture", picture: "/images/culture.jpg" }} />
-      </Link>
+      {console.log(tiles)}
+      {tiles}
     </div>
   );
 }
