728562d618179689ed4c3bd4c591bcf593dbd7a7
JOVE <jove@itu.dk>
2020-03-06 09:53:28 +0100

    DB connection works

--- joindenmark/src/page/Page.js
+++ joindenmark/src/page/Page.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Page.css";
+import { db } from "../firebase";
 import { NavBar } from "../components/navbar/NavBar";
 import { HomeButton } from "../components/homebutton/HomeButton";
 
 export function Page(props) {
+  const [title, setTitle] = useState(props.location.state.title);
+  const [headline, setHeadline] = useState('');
+  const dataFromDB = db.collection(title.toLowerCase()).get();
+
+  function getContentFromDB() {
+    dataFromDB.then(function(querySnapshot) {
+      querySnapshot.forEach(function(doc){
+        setHeadline(doc.data().headline)
+      });
+    });
+  }
+
+  useEffect(() => {
+    getContentFromDB();
+  }, []);
 
   return (
     <div>
-      <NavBar state={{ title: props.location.state.title }}></NavBar>
+      <NavBar state={{ title: title }}></NavBar>
       <img className="pagePicture" src={props.location.state.picture}/>
+      <p>{headline}</p>
       <HomeButton/>
     </div>
   );
