c79b7919ecdd8b671a5e4d77758a99e5d7e39d8c
JOVE <jove@itu.dk>
2020-03-06 12:15:44 +0100

    Added back icon

--- joindenmark/src/components/navbar/NavBar.js
+++ joindenmark/src/components/navbar/NavBar.js
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from "react";
 import "./NavBar.css";
 import { useHistory } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft} from "@fortawesome/free-solid-svg-icons";
 
 export function NavBar(props) {
   let history = useHistory();
 
   return (
     <div className="navbar">
-      <p onClick={() => history.goBack()} className="back">
-        Go Back
-      </p>
+      <FontAwesomeIcon icon={faChevronLeft} onClick={() => history.goBack()} className="back"/>
       <p className="title">{props.state.title}</p>
       <p className="search">Search</p>
     </div>
