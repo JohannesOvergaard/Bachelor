6722057e765ef4c460cba84e8b2dc38365c5190d
JOVE <jove@itu.dk>
2020-03-16 10:42:48 +0100

    Get search result and prints it in console if searching on portal

--- joindenmark/src/components/search/Search.js
+++ joindenmark/src/components/search/Search.js
@@ -1,16 +1,43 @@
 import React from "react";
 import "./Search.css";
+import {db} from "../../firebase";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimesCircle} from "@fortawesome/free-solid-svg-icons";
 
 export function Search({setShowSearch}){
+    const searchInAccommodation = async (search) => {
+        const snapshot = await db.collection('accommodation')
+            .where('keywords','array-contains', search.toLowerCase())
+            .get();
+        return snapshot.docs.reduce((acc, doc) => {
+            const data = doc.data();
+            return acc.concat(
+                <tr>
+                    <td>${data.headline}</td>
+                    {console.log(data.headline)}
+                </tr>);
+        }, '');
+    }
 
+    const textBoxSearch = document.querySelector('#searchInput');
+    const rowsSearch = document.querySelector('#rowsSearch');
+
+    //textBoxSearch.addEventListener('keyup', async (e) =>  await searchInAccommodation(e.target.value.toLowerCase()));
+    
     return (
         <div>
-            <input type="text" className="searchInput"/>
-            <div onClick={() => setShowSearch(false)}>
+            <input className="searchInput" type="text" id="searchInput" onChange={(e) => searchInAccommodation(e.target.value.toLowerCase())}/>
+            <div className="searchCloseIcon" onClick={() => setShowSearch(false)}>
                 <FontAwesomeIcon icon={faTimesCircle}/>
             </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Headline</th>
+                    </tr>
+                </thead>
+                <tbody id="rowsSearch"/>
+            </table>
         </div>
     );
 }
