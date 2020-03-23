f9a3a6828af61808c3114e8e2eecd4e0c72f876d
JOVE <jove@itu.dk>
2020-03-23 09:35:47 +0100

    removed innerHTML to return react object instead

--- joindenmark/src/components/search/Search.js
+++ joindenmark/src/components/search/Search.js
@@ -1,43 +1,42 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Search.css";
 import {db} from "../../firebase";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimesCircle} from "@fortawesome/free-solid-svg-icons";
 
 export function Search({setShowSearch}){
+    const [searchResults, setSearchResults] = useState([]);
     
     const searchInAccommodation = async (search) => {
         const snapshot = await db.collection('search')
             .where('keywords','array-contains', search.toLowerCase())
             .get();
-        return snapshot.docs.reduce((acc, doc) => {
-            const data = doc.data();
-            return acc.concat(`
-                <tr>
-                    <td>${doc.id}</td>
-                </tr>`);
-        }, '');
+        setSearchResults(
+            snapshot.docs.reduce((acc, doc) => {
+                const data = doc.data();
+                return acc.concat(
+                    <p 
+                        key={doc.id} 
+                        onClick={() => console.log(data.collection)}
+                    >
+                        {doc.id}
+                    </p>
+                );
+            }, [])
+        );
     }
 
-    const setRows = async (e) => {
-        document.getElementById('rowsSearch').innerHTML = 
-        await searchInAccommodation(e.target.value)
-    }
-    
-    
     return (
         <div>
             <input 
                 className="searchInput" 
                 type="text" 
-                onChange={(e) => setRows(e)}
+                onChange={(e) => searchInAccommodation(e.target.value)}
             />
             <div className="searchCloseIcon" onClick={() => setShowSearch(false)}>
                 <FontAwesomeIcon icon={faTimesCircle}/>
             </div>
-            <table>
-                <tbody id="rowsSearch"/>
-            </table>
+            {searchResults}
         </div>
     );
 }
