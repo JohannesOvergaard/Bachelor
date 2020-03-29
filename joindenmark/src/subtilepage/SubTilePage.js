61dcedefd966dd47d4a414bb2e8876eba1a4cb78
Bijo <bijo@itu.dk>
2020-03-29 20:49:54 +0200

    subtile page also filters based on user settings

--- joindenmark/src/subtilepage/SubTilePage.js
+++ joindenmark/src/subtilepage/SubTilePage.js
@@ -4,13 +4,24 @@ import { NavBar } from "../components/navbar/NavBar";
 import { HomeButton } from "../components/homebutton/HomeButton";
 import { getContentFilterBySettings } from "../services/ContentService";
 import { TilesContainer } from "../components/tilesContainer/TilesContainer";
+import { useSelector } from "react-redux";
 
 export function SubTilePage(props) {
   const [title] = useState(props.location.state.title);
   const [tileTitles, setTileTitles] = useState({});
+  const settings = useSelector(state => {
+    return state.userState.settings;
+  });
+  const processTiles = async () => {
+    const tiles = await getContentFilterBySettings(
+      title.toLowerCase(),
+      settings
+    );
+    setTileTitles(tiles);
+  };
 
   useEffect(() => {
-    getContentFilterBySettings(title.toLowerCase()).then(setTileTitles);
+    processTiles();
   }, []);
 
   return (
