2ed2e90308fb43c9cbd09d896e85b046bd5c8000
Bijo <bijo@itu.dk>
2020-04-23 14:35:27 +0200

    setting cleanup

--- joindenmark/src/Util/Helpers.js
+++ joindenmark/src/Util/Helpers.js
@@ -7,11 +7,14 @@ export function trim(title) {
 }
 
 export function updateArray(enabled, array, element) {
+  //Find the index of the element to update
   const index = array.indexOf(element);
   if (enabled) {
+    //if the element is enabled do not save it, remove from array
     array.splice(index, 1);
     return array;
   } else {
+    //if the element is disabled save it, add it to the array
     array.push(element);
     return array;
   }
